a74c8368e81b56b194a64b11b95b815f28d0cb8f
Venkatramana Chittipaka <venkatramanachittipaka@Venkatramanas-MacBook-Air.local>
2024-05-31 09:59:08 -0400

    Second program

---
+++ src/UseArgument.java
@@ -0,0 +1,11 @@
+public class UseArgument {
+    public static void main(String[] args){
+
+        System.out.print("Hi ");
+        System.out.print(args[2]+","+args[1]+","+args[0]);
+        System.out.println(".How r you?");
+
+    }
+
+}
+
